e790d00e15b2ca6e2d5c4f8b76180083d3bf6f8c
ethanbwillis <ebwillis123@gmail.com>
2025-04-14 15:28:24 -0400

    added fetch user data

--- collegescavs/src/bridge.js
+++ collegescavs/src/bridge.js
@@ -160,6 +160,35 @@ async function login(email, password){
     }
 }
 
+async function fetchUserByEmail(email) {
+    const baseURL = "https://www.cise.ufl.edu/~h.gill/cis4930/in-class/Dev/users.php?";
+    const query = new URLSearchParams({
+        method: "post",
+        email
+    }).toString();
+    
+    const fullURL = `${baseURL}?${query}`;
+    
+    try {
+        const res = await fetch(fullURL);
+        const data = await res.json();
+    
+        if (Array.isArray(data) && data.length > 0) {
+        const user = {
+            email: data[0].email,
+            name: `${data[0].first_name} ${data[0].last_name}`,
+            rating: data[0].rating,
+        };
+        return user;
+        } else {
+        return null;
+        }
+    } catch (error) {
+        console.error("Could not retrieve user info: ", error);
+        return null;
+    }
+}
+
 async function uploadListing(email, title, description, price){
     const baseURL= "https://www.cise.ufl.edu/~h.gill/cis4930/in-class/Dev/addpost.php";
     const query = new URLSearchParams({
@@ -217,4 +246,4 @@ async function newUser(email, password, first, last){
     
 }
 
-export { fetchAllListings, fetchListing, fetchUserListings, login, uploadListing, newUser, categories };
+export { fetchAllListings, fetchListing, fetchUserListings, login, fetchUserByEmail, uploadListing, newUser, categories };
