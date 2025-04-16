ad384cc748d55945f35066521d0339a3d4e4085b
Mauricio Lara <m.lara1@ufl.edu>
2025-04-16 02:55:52 -0400

    Wishlist working God save the queen

--- collegescavs/src/context/WishlistContext.js
+++ collegescavs/src/context/WishlistContext.js
@@ -1,20 +1,71 @@
-import React, { createContext, useState, useContext } from 'react';
-
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import { useAuth } from './AuthContext'; 
 const WishlistContext = createContext();
 
+
+
 export const WishlistProvider = ({ children }) => {
+  const { user, logout } = useAuth(); 
   const [wishlist, setWishlist] = useState([]);
 
+  
+  useEffect(() => {
+    if (user) {
+      const userWishlistKey = `wishlist_${user.email}`; 
+      const storedWishlist = localStorage.getItem(userWishlistKey);
+
+      if (storedWishlist) {
+        setWishlist(JSON.parse(storedWishlist));  
+      }
+    }
+  }, [user]); 
+
+
+  const saveWishlistToLocalStorage = (updatedWishlist) => {
+    if (user) {
+      const userWishlistKey = `wishlist_${user.email}`;  
+
+      localStorage.setItem(userWishlistKey, JSON.stringify(updatedWishlist));  
+    }
+  };
+
+  
+  const clearWishlist = () => {
+    if (user) {
+      const userWishlistKey = `wishlist_${user.email}`;  
+      localStorage.removeItem(userWishlistKey);  
+    }
+
+    setWishlist([]);  
+  };
+
+  
   const addToWishlist = (product) => {
-    setWishlist((prevWishlist) => [...prevWishlist, product]);
+
+    setWishlist((prevWishlist) => {
+      const updatedWishlist = [...prevWishlist, product];
+      saveWishlistToLocalStorage(updatedWishlist);  
+      return updatedWishlist;
+    });
   };
 
   const removeFromWishlist = (id) => {
-    setWishlist((prevWishlist) => prevWishlist.filter(item => item.id !== id));
+    setWishlist((prevWishlist) => {
+      const updatedWishlist = prevWishlist.filter(item => item.id !== id);
+      saveWishlistToLocalStorage(updatedWishlist);  
+      return updatedWishlist;
+    });
   };
 
+  useEffect(() => {
+    if (!user) {
+      clearWishlist();  
+    }
+  }, [user]);  
+
+
   return (
-    <WishlistContext.Provider value={{ wishlist, addToWishlist, removeFromWishlist }}>
+    <WishlistContext.Provider value={{ wishlist, addToWishlist, removeFromWishlist, clearWishlist }}>
       {children}
     </WishlistContext.Provider>
   );
@@ -23,3 +74,5 @@ export const WishlistProvider = ({ children }) => {
 export const useWishlist = () => {
   return useContext(WishlistContext);
 };
+
+
